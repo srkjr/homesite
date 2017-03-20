c3c0b4d9870e824f8fd4fc0c6f4c8fccb038f9bc
srkjr <renelonstrup@gmail.com>
2017-03-20 23:14:09 +0100

    Started base of project v2

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/charlie/r3/myenv/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- r3/development_settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*- 
-
-from .settings import *
-
-DEBUG = True
-
-TEMPLATE_DEBUG = DEBUG
-
